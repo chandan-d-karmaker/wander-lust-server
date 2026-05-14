a14f8c1cd353dfb57e599dff630ce9b4b2e7ac6b
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-05-14 21:12:11 +0600

    feat: update JWKS URL to use environment variable and add Vercel configuration

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 });
 
 const JWKS = createRemoteJWKSet(
-  new URL("http://localhost:3000/api/auth/jwks")
+  new URL(`${process.env.CLIENT_URI}/api/auth/jwks`)
 )
 
 const verifyToken = async(req, res, next) => {
@@ -49,7 +49,7 @@ const verifyToken = async(req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("wanderlust");
     const destinationCollection = db.collection("destination");
@@ -115,7 +115,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
